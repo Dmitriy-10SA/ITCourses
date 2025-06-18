dbb91f444ec3b0095e8dfaa5c3fc385bf7bb4a7d
dsemkin <semkin_dmitriy10@vk.com>
2025-06-18 12:04:04 +0400

    UiButton ready

--- design/src/main/java/com/andef/itcourses/design/button/ui/UiButton.kt
+++ design/src/main/java/com/andef/itcourses/design/button/ui/UiButton.kt
@@ -1,16 +1,23 @@
 package com.andef.itcourses.design.button.ui
 
+import android.content.Intent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import com.andef.itcourses.design.R
 import com.andef.itcourses.design.button.type.UiButtonType
 import com.andef.itcourses.design.ui.theme.DarkGray
@@ -18,37 +25,32 @@ import com.andef.itcourses.design.ui.theme.Green
 import com.andef.itcourses.design.ui.theme.White
 
 @Composable
-fun UiButton(
-    modifier: Modifier = Modifier,
-    text: String,
-    type: UiButtonType,
-    onClick: () -> Unit
-) {
+fun UiButton(modifier: Modifier = Modifier, type: UiButtonType) {
     when (type) {
-        UiButtonType.DarkGray -> {
+        is UiButtonType.DarkGray -> {
             ColorUiButton(
                 modifier = modifier,
-                text = text,
-                onClick = onClick,
+                text = type.text,
+                onClick = type.onClick,
                 containerColor = DarkGray
             )
         }
 
-        UiButtonType.Green -> {
+        is UiButtonType.Green -> {
             ColorUiButton(
                 modifier = modifier,
-                text = text,
-                onClick = onClick,
+                text = type.text,
+                onClick = type.onClick,
                 containerColor = Green
             )
         }
 
         UiButtonType.Odnoklassniki -> {
-            OdnoklassnikiUiButton()
+            OdnoklassnikiUiButton(modifier = modifier)
         }
 
         UiButtonType.VK -> {
-            VKUiButton()
+            VKUiButton(modifier = modifier)
         }
     }
 }
@@ -61,9 +63,13 @@ private fun ColorUiButton(
     contentColor: Color = White,
     onClick: () -> Unit
 ) {
+    val gradient = Brush.verticalGradient(colors = listOf(containerColor, containerColor))
     Button(
-        modifier = modifier,
-        colors = ButtonDefaults.buttonColors(containerColor = containerColor, contentColor),
+        modifier = modifier.background(brush = gradient, shape = shape),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = contentColor
+        ),
         shape = shape,
         onClick = onClick
     ) {
@@ -72,13 +78,49 @@ private fun ColorUiButton(
 }
 
 @Composable
-private fun OdnoklassnikiUiButton() {
-
+private fun OdnoklassnikiUiButton(modifier: Modifier) {
+    val context = LocalContext.current
+    val gradient = Brush.verticalGradient(colors = listOf(Color(0xFFF98509), Color(0xFFF95D00)))
+    Button(
+        modifier = modifier.background(brush = gradient, shape = shape),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = White
+        ),
+        onClick = {
+            val intent = Intent(Intent.ACTION_VIEW, "https://ok.ru/".toUri())
+            context.startActivity(intent)
+        },
+        shape = shape
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.odnoklassniki),
+            contentDescription = "Значок одноклассников"
+        )
+    }
 }
 
 @Composable
-private fun VKUiButton() {
-
+private fun VKUiButton(modifier: Modifier) {
+    val context = LocalContext.current
+    val gradient = Brush.verticalGradient(colors = listOf(Color(0xFF2683ED), Color(0xFF2683ED)))
+    Button(
+        modifier = modifier.background(brush = gradient, shape = shape),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = White
+        ),
+        onClick = {
+            val intent = Intent(Intent.ACTION_VIEW, "https://vk.com/".toUri())
+            context.startActivity(intent)
+        },
+        shape = shape
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.vk),
+            contentDescription = "Значок вк"
+        )
+    }
 }
 
 private val shape = RoundedCornerShape(size = 30.dp)
